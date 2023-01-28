32a56630dbce1694b37f514c9fca079683993524
Christiaan <christiaanlemmer@hotmail.com>
2023-01-28 14:52:40 -0700

    Update sdConverter.py

--- sdConverter.py
+++ sdConverter.py
@@ -1,2 +1,3 @@
 # Format will be as follows:
 # Will be a plain text file with each line containing
+# What you need to do is read the file collect all data, make a nice excel and maybe some matplotlib to make some graphs
