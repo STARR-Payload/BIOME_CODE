34e519915f2f8840f85141afbd26018c238d3774
Christiaan <christiaanlemmer@hotmail.com>
2023-01-30 19:04:29 -0700

    Create testerfile.ino

---
+++ testerfile/testerfile.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(8,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(8, HIGH);
+  delay(2000);
+  digitalWrite(8,LOW);
+  delay(2000);
+
+}
