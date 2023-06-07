47713e3d8b0a8ef596e61f5704eee9d4e6b3bdb3
bpaul2 <paulson2@ualberta.ca>
2023-06-06 20:29:48 -0600

    Added temperature sensor functionality

--- main/softwareTeam.h
+++ main/softwareTeam.h
@@ -1,32 +1,49 @@
-/*
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-This is the header file I need done from software I will try my best to layout what I want with functions 
-Feel free to change params returns need to stay the same and if you add other functions just make them static 
+// Define precision of temperature in bits
+#define TEMP_PRECISION 12
 
-*/
+// Define i/o pins for valves (index 0 corresponds to valve 1's pin and so forth)
+OneWire sensorPins[] = {6, 5, 3, 2};
+const int numOfSensors = sizeof(sensorPins) / sizeof(OneWire);
 
+// Define DallasTemperature instances for sensors
+DallasTemperature sensors[numOfSensors];
 
+// Determines if a sensor has been initialized (1 = Yes, 0 = No)
+int isIni[numOfSensors] = {0};
+
+// Initializes all valve sensors
+void airflowSetupAll() {
+    Serial.println("Initializing all valve sensors...");
+    for (int i = 1; i <= numOfSensors; i++) {
+        airflowSetup(i);
+    }
+    Serial.println("All valve sensors are initialized.");
+}
+
+// Initializes individual valve sensors
 void airflowSetup(int valveNumber) {
-    /* 
+    Serial.print("Initializing valve " + String(valveNumber) + ": ");
 
-    Will setup all objects or however you want to do it
-    This one will be a beefy one since you need to do all the low level init 
-        https://www.pjrc.com/teensy/td_libs_OneWire.html < will be real helpful for setup 
+    // Initialize sensor if it hasn't been already
+    int index = valveNumber-1;
+    if (!isIni[index]) {
+        DeviceAddress deviceAddress;
 
-    */
+        sensors[index].setOneWire(&sensorPins[index]);
+        sensors[index].begin();
+        if (sensors[index].getAddress(deviceAddress, 0)) sensors[index].setResolution(deviceAddress, TEMP_PRECISION);
+        isIni[index] = 1;
+    }
 
-   // CODE HERE
-
+    Serial.println("Done");
 }
 
+// Return temperature of individual valve sensor
 float airflowTemp(int valveNumber) {
-    /*
-    
-    returns the temp reading of the corresponding valve
-    
-    */
-
-   // CODE HERE
-
+    int index = valveNumber-1;
+    sensors[index].requestTemperatures();
+    return sensors[index].getTempCByIndex(0);
 }
-
